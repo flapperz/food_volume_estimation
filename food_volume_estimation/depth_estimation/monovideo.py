bc2a5e0201c99a063050a6ccb58c5ecf77fcffe4
Krit Cholapand <flap.atyz@gmail.com>
2021-05-13 06:01:34 +0700

    🩹 Make training script work

--- food_volume_estimation/depth_estimation/monovideo.py
+++ food_volume_estimation/depth_estimation/monovideo.py
@@ -1,3 +1,6 @@
+import sys, os
+print(os.path.dirname(os.path.realpath(__file__)))
+
 import os
 import argparse
 import numpy as np
@@ -7,8 +10,8 @@ from keras.models import Model
 from keras.optimizers import Adam
 from keras.callbacks import LearningRateScheduler, LambdaCallback
 import keras.preprocessing.image as pre
-from food_volume_estimation.depth_estimation.custom_modules import *
-from food_volume_estimation.depth_estimation.networks import NetworkBuilder
+from custom_modules import *
+from networks import NetworkBuilder
 
 
 class MonovideoModel:
--- food_volume_estimation/depth_estimation/networks.py
+++ food_volume_estimation/depth_estimation/networks.py
@@ -4,7 +4,7 @@ from keras.models import Model
 from keras.layers import Conv2D, UpSampling2D, GlobalAveragePooling2D, \
     LeakyReLU, Dense, Flatten, Input, Concatenate, Lambda, BatchNormalization
 from classification_models.keras import Classifiers
-from food_volume_estimation.depth_estimation.custom_modules import *
+from custom_modules import *
 
 ResNet18, _ = Classifiers.get('resnet18')
 
